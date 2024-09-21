38ee868fa04d3a6fb0b1135186a712138dad6447
Dipo Yudhis Rana <dipoyudhisrana@gmail.com>
2024-09-22 01:24:50 +0700

    update 21/09/2024

--- models/entity/users.go
+++ models/entity/users.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Users struct {
-	Id        uint				`json:"id" gorm:"primaryKey"`
-	Name      string 			`json:"name" gorm:"type:varchar(100);not null"`
-	Email     string 			`json:"email" gorm:"type:varchar(100);not null"`
-	CreatedAt time.Time 		`json:"created_at"`
-	UpdatedAt time.Time			`json:"updated_at"`
-	DeletedAt gorm.DeletedAt	`json:"deleted_at" gorm:"index"`
+	Id        uint   `json:"id" gorm:"primaryKey"`
+	Name      string `json:"name" gorm:"type:varchar(100);not null"`
+	Email     string `json:"email" gorm:"type:varchar(100);not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
